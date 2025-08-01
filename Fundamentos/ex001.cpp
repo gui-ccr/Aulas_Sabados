737aae20a95b441113885e4a1a6b4d2a39f472d3
Guilherme <116519824+gui-ccr@users.noreply.github.com>
2025-08-01 00:10:50 -0300

    Refactor variable examples and output formatting in ex001.cpp
    Commented out unused variable declarations and example outputs, added clarifying comments, and updated output formatting for boolean variables. Also added missing #include <string> for string usage.

--- Fundamentos/ex001.cpp
+++ Fundamentos/ex001.cpp
@@ -1,50 +1,50 @@
 // tipos de variaveis simples
 
-#include <iostream>
+#include <iostream> // biblioteca 
+#include <string>
 using namespace std;
 
- main()
-{
+ main(){
     // inteiro (numero todo)
-    int idade = 21 ;
-    int ano = 2025;
-    int dias = 23.5;
-    int dia = 23;
+    // int idade = 21 ;
+    // int ano = 2025;
+    // int dias = 23.5;
+    // int dia = 23;
 
-    // std::cout << dias;
-    
+    // std::cout << dia;
+
     // double (numero que inclui casas decimais)
-    double preco = 12.5;
-    double temperatura = 27.32;
+    // double preco = 12.5;
+    // double temperatura = 27.32;
     
-    // std::cout << temperatura;
+    // // std::cout << temperatura;
 
-    // char (unico caracter)
-    // char nota = "A";
-    char inicial = 'M'; // nota-se que um unico carartere utiliza-se aspas simples ('')
+    // // char (unico caracter)
+    // // char nota = "A";
+    // char um;
 
-    // std::cout << inicial;
 
-    // boleano (verdadeiro ou falso)
+    // nota-se que um unico carartere utiliza-se aspas simples ('')
+
+    // // std::cout << inicial;
+
+    // // boleano (verdadeiro ou falso)
     bool bonita = true;
     bool inteligente = true;
     bool preguicosa = false;
 
-    // std::cout << bonita;
+    // // std::cout << bonita;
 
-    // string (uma serie de carateres simbolizado por aspas duplas ("") )
+    // // string (uma serie de carateres simbolizado por aspas duplas ("") )
 
-    std::string nome = "Milena";
+    // std::string nome = "Milena";
 
-    std::cout << "hoje e dia " << dia << " e " << nome << " esta aprendendo a codificar em c++, ela tem " << idade << " anos a inicial do nome dela e (" << inicial << ")" << endl;
+    // std::cout << "hoje e dia " << dia << " e " << nome << " esta aprendendo a codificar em c++, ela tem " << idade << " anos a inicial do nome dela e (" << inicial << ")" << endl;
 
-    cout << "milena e bonita ?: " << bonita << endl;
-    cout << "milena e inteligente ?: " << inteligente << endl;
-    cout << "milena e preguicosa?: " << preguicosa << endl;
+    cout << "milena e bonita: " << bonita << endl;
+    cout << "milena e inteligente: " << inteligente << endl;
+    cout << "milena e preguicosa: " << preguicosa << endl;
 
-
-
-    
     
     
     return 0;
